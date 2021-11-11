9827c5caae21c590a96a1ab3dddb0acbe4371dd0
Covlea-Pop <covleacc@gmail.com>
2021-11-12 00:52:27 +0200

    first commit

---
+++ Features/ABTesting.feature
@@ -0,0 +1,8 @@
+Feature: As a user i want to access AB Testing page
+
+    Scenario: Access AB testing page
+
+      Given I open browser
+      When i click on the AB Testing link
+      And a new page is open
+      Then i quit page
---
+++ src/test/java/stepDefinitions/ABTesting.java
@@ -0,0 +1,44 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class ABTesting {
+    WebDriver driver;
+
+    @Given("I open browser")
+    public void i_open_browser() {
+        System.setProperty("webdriver.chrome.driver","D:\\IT\\SDA\\Programe\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+
+    }
+
+    @When("i click on the AB Testing link")
+    public void i_click_on_the_ab_testing_link() {
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[1]/a")).click();
+
+
+    }
+
+    @When("a new page is open")
+    public void a_new_page_is_open() {
+        String status = driver.findElement(By.xpath("/html/body/div[2]/div/div/h3")).getText();
+        System.out.println(status);
+
+
+    }
+
+    @Then("i quit page")
+    public void i_quit_page() {
+
+        driver.quit();
+
+
+    }
+
+}
